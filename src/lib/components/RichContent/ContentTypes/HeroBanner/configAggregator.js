6c10e1465f4288f6553e391d7a4e7bbc9ada38d7
Marcin Kwiatkowski <contact@frodigo.com>
2020-11-16 08:22:27 +0100

    Created config aggregator and component

--- src/lib/components/RichContent/ContentTypes/HeroBanner/configAggregator.js
+++ src/lib/components/RichContent/ContentTypes/HeroBanner/configAggregator.js
@@ -1,4 +1,17 @@
-export default (node, props) => {
+import { getAdvanced } from "@magento/pagebuilder/lib/utils";
+
+export default (node) => {
+    const desktopImageElement = node.querySelector('[data-element="desktop_image"]');
+    const mobileImageElement = node.querySelector('[data-element="mobile_image"]');
+    const headingElement = node.querySelector('[data-element="heading"]');
+    const buttonElement = node.querySelector('[data-element="button_url"]');
+
     return {
+        desktopImage: desktopImageElement.getAttribute('src'),
+        mobileImage: mobileImageElement.getAttribute('src'),
+        heading: headingElement.innerText,
+        buttonText: buttonElement.innerText,
+        buttonUrl: buttonElement.getAttribute('href'),
+        ...getAdvanced(node)
     };
 };
--- src/lib/components/RichContent/ContentTypes/HeroBanner/heroBanner.js
+++ src/lib/components/RichContent/ContentTypes/HeroBanner/heroBanner.js
@@ -1,17 +1,62 @@
 import React from 'react';
 import { mergeClasses } from '@magento/venia-ui/lib/classify';
 import defaultClasses from './heroBanner.css';
-import { shape, string } from 'prop-types';
+import {arrayOf, shape, string} from 'prop-types';
 
-
-
-// Component for testing setup
 const HeroBanner = props => {
+    console.clear();
+    console.log(props);
+
     const classes = mergeClasses(defaultClasses, props.classes);
+    const {
+        desktopImage,
+        mobileImage,
+        heading,
+        buttonText,
+        buttonUrl,
+        textAlign,
+        border,
+        borderColor,
+        borderWidth,
+        marginTop,
+        marginRight,
+        marginBottom,
+        marginLeft,
+        paddingTop,
+        paddingRight,
+        paddingBottom,
+        paddingLeft,
+        cssClasses
+    } = props;
+    const rootStyles = {
+        textAlign,
+        border,
+        borderColor,
+        borderWidth,
+        marginTop,
+        marginRight,
+        marginBottom,
+        marginLeft,
+        paddingTop,
+        paddingRight,
+        paddingBottom,
+        paddingLeft
+    };
+
+    let image = desktopImage;
+    if (mobileImage && window.matchMedia('(max-width: 768px)').matches) {
+        image = mobileImage;
+    }
+
     return (
-        <div className={classes.heroBlock}>
-            <div>Content Type: {props.contentType}</div>
-            <div>Appearance: {props.appearance}</div>
+        <div className={[classes.heroBlock, ...cssClasses].join(' ')} style={rootStyles}>
+            <figure className={classes.heroBlockFigure}>
+                <img className={classes.heroBlockImage} src={image} alt="figure"/>
+            </figure>
+            <section className={classes.heroBlockTape}>
+                <h2 className={classes.heroBlockHeading}>{heading}</h2>
+                <a className={classes.heroBlockAction} href={buttonUrl}>{buttonText}</a>
+            </section>
         </div>
     );
 };
@@ -21,7 +66,24 @@ HeroBanner.propTypes = {
         heroBlock: string
     }),
     contentType: string,
-    appearance: string
+    appearance: string,
+    buttonText: string.isRequired,
+    buttonUrl: string.isRequired,
+    desktopImage: string.isRequired,
+    heading: string.isRequired,
+    textAlign: string,
+    border: string,
+    borderColor: string,
+    borderWidth: string,
+    marginTop: string,
+    marginRight: string,
+    marginBottom: string,
+    marginLeft: string,
+    paddingTop: string,
+    paddingRight: string,
+    paddingBottom: string,
+    paddingLeft: string,
+    cssClasses: arrayOf(string)
 };
 
 export default HeroBanner;
